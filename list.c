4efeb8f425f91d19dc3999aa3ad4b61706a6aac8
Your Name <you@example.com>
2022-03-20 22:26:02 -0300

    new try

--- list.c
+++ list.c
@@ -139,18 +139,19 @@ void * popCurrent(List * list) {
     //
      if(list->current->next != NULL){
         list->current->next->prev = list->current->prev;
+        return list->current->data;
     }
     else{
         list->current->prev->next = NULL;
         list->tail = list->current->prev;
+        return list->current->data;
     } 
     //
     if( list->current->prev != NULL ){
         list->current->prev->next = list->current->next;
     }
     else {list->head = list->current->next;
-      list->current->prev->next = list->current->next;
-    list->current->next->prev = list->current->prev;
+      
     }
   
 
